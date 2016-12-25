9ab98656e296663b124c1e5d933e6f4c8d18b8db
aandino11 <aandino11@gmail.com>
2016-12-25 00:39:08 -0500

    setup frameworks and UI

---
+++ Watson-testapp/Watson-testapp/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  Watson-testapp
+//
+//  Created by Alvin  Andino  on 12/24/16.
+//  Copyright © 2016 Alvin  Andino . All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+  
+  @IBOutlet weak var WatsonReplyLabel: UILabel!
+  @IBOutlet weak var RequestLabel: UILabel!
+
+  @IBAction func ButtonPressed(_ sender: UIButton) {
+  }
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
